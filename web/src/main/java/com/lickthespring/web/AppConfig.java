52f7bdb464f4b896e2485dd29b9b8bf9a79aaf03
YouHoGeon <me@youhogeon.com>
2022-12-30 22:17:58 +0900

    spring web mvc CH6

---
+++ web/src/main/java/com/lickthespring/web/AppConfig.java
@@ -0,0 +1,10 @@
+package com.lickthespring.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+    
+}
---
+++ web/src/main/java/com/lickthespring/web/MyWebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package com.lickthespring.web;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(AppConfig.class);
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
